ac51ed91cac26df9ea7a21581ff5eecc8e63bdd7
Rachel Tucker <rach.mello@hotmail.com>
2023-03-01 14:50:05 -0600

    using Absctract Base Class to enforce constraints on other classes.

--- inheritance_start.py
+++ inheritance_start.py
@@ -1,25 +1,34 @@
 #Book, Magazine and Newspaper all have a title and price. We can better this code by adding inheritance.
+#below is a "class hierarchy"
 
-class Book:
-    def __init__(self, title, author, pages, price):
+
+#create a class with title and price so other classes can inherit from it
+class Publication:
+    def __init__(self, title, price):
         self.title = title
+        self.price = price
+
+#create a class that inherits from Publication but has other attributes for Magazine and Newspaper
+class Periodical(Publication):
+    def __init__(self, title, price, period, publisher):
+        super().__init__(title, price)
+        self.period = period
+        self.publisher = publisher
+
+class Book(Publication):
+    def __init__(self, title, author, pages, price):
+        super().__init__(title, price)
         self.author = author
         self.pages = pages
-        self.price = price
 
-class Magazine:
-    def __init__(self, title, publisher, price, period):
-        self.title = title
-        self. publisher = publisher
-        self.price = price
-        self.period = period
 
-class Newspaper:
+class Magazine(Periodical):
     def __init__(self, title, publisher, price, period):
-        self.title = title
-        self.publisher = publisher
-        self.price = price
-        self.period = period
+        super().__init__(title, price, period, publisher)
+
+class Newspaper(Periodical):
+    def __init__(self, title, publisher, price, period):
+        super().__init__(title, price, period, publisher)
     
 b1 = Book("Brave New World", "Aldous Huxley", 311, 29.0)
 n1 = Newspaper("NY Times", "New York Times Company", 6.0, "daily")
